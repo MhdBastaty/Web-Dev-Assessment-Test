e383c65b7e92a36c8e928568e995f14921b28914
Mohammad Bastaty <72391469+MhdBastaty@users.noreply.github.com>
2022-03-04 10:19:45 +0800

    make the connection with DB, add .env file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
-const app = express()
-const port = 6000
+const app = express();
+const dotenv = require ('dotenv');
+const mysql = require ('mysql');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+dotenv.config();
+
+// Create the Connection with DB
+const db = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
+
+//Connect 
+
+db.connect((err) => {
+    if(err){
+        console.log('something wrong with db');
+    }
+        console.log('DBConnection is Successful')
+});
+
+
+app.listen(process.env.PORT || 6000, () => {
+  console.log(`Backend Server Is Running on ${process.env.PORT}`)
 })
