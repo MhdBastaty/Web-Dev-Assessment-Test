d9e633152c88b45557019363160725f6a33850a1
Mohammad Bastaty <72391469+MhdBastaty@users.noreply.github.com>
2022-03-06 00:28:09 +0800

    modified the files

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,14 +1,47 @@
 // const create = require ('../service/userService');
 
-const { hashSync, genSaltSync } =require ('bcrypt')
+const CryptoJS = require("crypto-js"); // crypt the password
+const bcrypt = require('bcrypt');
+const User = require ('../models/User');
+require ('dotenv').config();
+
+let hashedPass = '';
 
 
 exports.getAllUsers = async (req, res, next) => {
     res.json("getall users")
 };
 
+// exports.register = async (req, res, next) => {
+//     hashedPass = await bcrypt.hash(req.body.password,10).toString();
+    
+
+//     const {user_name, email, password} = req.body;
+//     let user = new User (user_name, email, password);
+
+//     user = await user.save();
+//     res.json(user)
+// };  
+
+
 exports.register = async (req, res, next) => {
-    res.json("register")
+    // hashedPass = await bcrypt.hash(req.body.password,10).toString(CryptoJS.enc.Utf8)
+    
+    const newUser = new User ({
+        
+        user_name:req.body.user_name,
+        email: req.body.email,
+        password: req.body.password
+        //  = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    })
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+        // console.log(savedUser)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 };  
 
 exports.login = async (req, res, next) => {
--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,43 @@
+const db = require ('../config/config_db');
 //class syntax in Es6
 // Allows me to use different db methods
 class User {
-    con
+    constructor( user_name, email, password, date_of_birth, phone_number, address) {
+        
+        this.user_name = user_name;
+        this.email = email;
+        this.password = password;
+        this.date_of_birth = date_of_birth;
+        this.phone_number = phone_number;
+        this.address = address;
+    }
+
+// only accesible by this class
+    async save() {
+        let d = new Date();
+        let yyy = d.getFullYear();
+        let mm = d.getMonth() + 1;   // cuz the month in js 0 index
+        let dd = d.getDate()                   // return the actual day in the month not in year 
+
+        let createdAtDate = `${yyy}-${mm}-${dd}`;
+        
+        let sql = `
+        INSERT INTO users (
+            user_name,
+            email,
+            password
+        )
+        VALUES (
+           ' ${this.user_name}',
+           ' ${this.email}',
+           ' ${this.password}'
+        )
+        `;
+
+        const [newUser, _] = await db.execute(sql);
+        return newUser;
+    }
+
 }
+
+module.exports = User;
